9b4d365ad2354663c3e5df666e0169e5b2793c13
Bart <bartvuden@hotmail.com>
2015-05-21 11:58:57 +0200

    add infrastructure project and add first properties of feature entity

--- DMi.Vision/src/DMi.Vision.Models/Feature.cs
+++ DMi.Vision/src/DMi.Vision.Models/Feature.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DMi.Infrastructure;
 
 namespace DMi.Vision.Models
 {
-    // This project can output the Class library as a NuGet Package.
-    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-    public class Feature
+    
+    public class Feature : DomainEntity<int>, IDateTracking
     {
-        public Feature()
+        private string _title;
+        private string _description; 
+
+        public Feature(string title, string description)
         {
+            _title = title;
+            _description = description;
         }
+
+        /// <summary>
+        /// Gets or sets the date and time the feature was created.
+        /// </summary>
+        public DateTime DateCreated { get; set; }
+        /// <summary>
+        /// Gets or sets the date and time the feature was last modified.
+        /// </summary>
+        public DateTime DateModified { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Points { get; set; }
     }
+
+    
+
 }
